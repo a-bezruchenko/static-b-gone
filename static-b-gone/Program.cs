2a4f8f8c0b195df9c04b6617166e5a18f00e1dda
Aleksey Bezruchenko <Aleksey.Bezruchenko@itl.team>
2022-05-19 12:46:27 +0300

    Add project files.

---
+++ static-b-gone/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace static_b_gone
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            const string path = "C:\\projects\\ramplus\\RamPlusSQL.sln";
+
+            MSBuildLocator.RegisterDefaults();
+
+            var workspace = MSBuildWorkspace.Create();
+            var solution = await workspace.OpenSolutionAsync(path);
+            foreach (var project in solution.Projects)
+            {
+                foreach (var file in project.Documents)
+                {
+                    Console.WriteLine(file.Name);
+                }
+            }
+
+        }
+    }
+}
