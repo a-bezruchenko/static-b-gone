43d90865c6266d640bf0e4bd2ba4a05bb08ed750
Aleksey Bezruchenko <Aleksey.Bezruchenko@itl.team>
2022-05-19 16:00:41 +0300

    Test: searching for specified class in file names

--- static-b-gone/Program.cs
+++ static-b-gone/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Build.Locator;
 using Microsoft.CodeAnalysis.MSBuild;
+using static_b_gone.Util;
 
 namespace static_b_gone
 {
@@ -9,7 +10,12 @@ namespace static_b_gone
     {
         public static async Task Main(string[] args)
         {
-            const string path = "C:\\projects\\ramplus\\RamPlusSQL.sln";
+            var parsedArgs = new CmdLineParser().ParseArgs(args);
+            //var configArgs;
+            //const string path = "C:\\projects\\ramplus\\RamPlusSQL.sln";
+
+            string path = parsedArgs["path"];
+            string classToReplace = parsedArgs["class"];
 
             MSBuildLocator.RegisterDefaults();
 
@@ -19,7 +25,8 @@ namespace static_b_gone
             {
                 foreach (var file in project.Documents)
                 {
-                    Console.WriteLine(file.Name);
+                    if (file.Name.Contains(classToReplace))
+                        Console.WriteLine(file.Name);
                 }
             }
 
---
+++ static-b-gone/Util/CmdLineParser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_b_gone.Util
+{
+    public class CmdLineParser
+    {
+        private readonly string[] argNamePrefixes = { "--", "-", "/" };
+        private const string emptyValueFiller = "";
+
+        public Dictionary<string, string> ParseArgs(string[] args)
+        {
+            bool prevIsName = false;
+            string prevValue = "";
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            bool parsedSuccessfully = true;
+
+            foreach (var arg in args)
+            {
+                if (IsArgName(arg))
+                {
+                    if (prevIsName)
+                    {
+                        result[prevValue] = emptyValueFiller;
+                    }
+
+                    prevIsName = true;
+                    prevValue = RemoveArgNamePrefix(arg.ToLower());
+                }
+                else
+                {
+                    if (prevIsName)
+                    {
+                        result[prevValue] = arg;
+                        prevIsName = false;
+                    }
+                    else
+                    {
+                        parsedSuccessfully = false;
+                        break;
+                    }
+                }
+            }
+
+            if (parsedSuccessfully)
+                return result;
+            else
+                return null;
+        }
+
+        private bool IsArgName(string arg)
+        {
+            foreach (var start in argNamePrefixes)
+            {
+                if (arg.StartsWith(start))
+                    return true;
+            }
+            return false;
+        }
+
+        private string RemoveArgNamePrefix(string arg)
+        {
+            foreach (var start in argNamePrefixes)
+            {
+                if (arg.StartsWith(start))
+                    return arg.Replace(start, null);
+            }
+            return arg;
+        }
+    }
+}
